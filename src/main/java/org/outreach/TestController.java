e5dde32775676819e25990c2029f782e1b74e2d9
Richard Turner <richard.turner@outreach.io>
2018-09-17 15:50:06 -0700

    Add query param and header test endpoint

--- src/main/java/org/outreach/TestController.java
+++ src/main/java/org/outreach/TestController.java
@@ -1,10 +1,13 @@
 package org.outreach;
 
+import org.jooby.Mutant;
+import org.jooby.Request;
 import org.jooby.mvc.Consumes;
 import org.jooby.mvc.GET;
 import org.jooby.mvc.Path;
 import org.jooby.mvc.Produces;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Consumes("application/json")
@@ -15,5 +18,26 @@ public class TestController {
     @Path("/env")
     public Map<String, String> getEnv() {
         return System.getenv();
+    }
+
+    @GET
+    @Path("/test")
+    public Map<String, Object> getTestData(Request request) {
+        Map<String, Object> result = new HashMap<>();
+
+        Map<String, String> headerMap = new HashMap<>();
+        for (String header : request.headers().keySet()) {
+            Mutant value = request.headers().get(header);
+            headerMap.put(header, value.value("[failed_to_convert_to_string]"));
+        }
+        result.put("headers", headerMap);
+        Map<String, String> paramMap = new HashMap<>();
+        for (String paramKey : request.params().toMap().keySet()) {
+            Mutant value = request.params().toMap().get(paramKey);
+            paramMap.put(paramKey, value.toString());
+        }
+
+        result.put("queryParams", paramMap);
+        return result;
     }
 }
